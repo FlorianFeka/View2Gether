1afff1c45bce6908d382b54fcda03d7807a89127
FlorianFeka <florianfeka05@gmail.com>
2020-07-15 17:42:38 +0200

    refactor

--- Frontend/src/app/services/socket.service.ts
+++ Frontend/src/app/services/socket.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Command } from '../models/Command';
 import { Observable } from 'rxjs';
 import * as io from 'socket.io-client';
 
-// TODO -> put url in environment file
-const SERVER_URL = 'http://localhost:8000'
+import { environment } from '../../environments/environment';
+import { Command } from '../models/Command';
+
+const SERVER_URL = environment.socketUrl;
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +13,23 @@ const SERVER_URL = 'http://localhost:8000'
 export class SocketService {
   private socket;
 
-  initSocket(){
+  constructor() {
     this.socket = io(SERVER_URL);
   }
 
-  joinRoom(roomId: string){
-    this.socket.emit('joinRoom', roomId)
+  joinRoom(roomId: string) {
+    this.socket.emit('joinRoom', roomId);
   }
 
-  sendCommandToRoom(command: Command){
-    const value = { room: command.roomId,  action: command.action, value: command.value}
-    this.socket.emit('sendCommandToRoom', value)
+  sendCommandToRoom(roomId: string, command: Command) {
+    this.socket.emit('sendCommandToRoom', roomId, command);
   }
 
   onCommand(): Observable<Command> {
-    return new Observable<Command>(observer => {
-        this.socket.on('command', (data: Command) => observer.next(data));
+    return new Observable<Command>((observer) => {
+      this.socket.on('command', (data: Command) => {
+        observer.next(data);
+      });
     });
   }
 }
